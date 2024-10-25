aba006ecb31e53ac39ea8a04e724a0602dc6782f
Tom <tommy_brown@me.com>
2024-10-25 14:47:48 +0100

    Added itt/edit/reason DIR and boilerplate files

---
+++ app/routes/edit/itt-route.js
@@ -0,0 +1,27 @@
+module.exports = router => {
+
+    router.post('/itt/establishment', (req, res) => {
+      if (req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+      } else {
+        res.redirect('/itt/start-date')  
+      }      
+    })
+
+    router.post('/itt/start-date', (req, res) => {
+      if (req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+      } else {
+        res.redirect('/itt/check')  
+      }      
+    })
+
+    /////////////  FLASH //////////////
+    router.post('/itt/check-handler', (req, res) => {
+
+      req.flash('success', 'Route to professional status added')
+
+      res.redirect('/route')
+    })
+
+}
