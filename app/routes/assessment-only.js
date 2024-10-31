8b464495029380ce96b3d9f9e7517c75db6730b0
Tom <tommy_brown@me.com>
2024-10-31 16:55:49 +0000

    routes/index  - added ass only conditional templates for route + route and qual

--- app/routes/assessment-only.js
+++ app/routes/assessment-only.js
@@ -48,4 +48,24 @@ module.exports = router => {
     res.redirect('/route')
   })
 
+    /////////////  ADD QUALS ROUTES //////////////
+    router.post('/assessment-only/add-qualification/award-date', (req, res) => {
+
+      if (req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+      } else {
+        res.redirect('/assessment-only/add-qualification/check')  
+      }      
+    })
+
+    router.post('/assessment-only/add-qualification/check-handler', (req, res) => {
+
+      req.flash('success', 'Professional status added')
+
+      let data = req.session.data
+      data['assOnlyAddQualDate'] = data['assOnlyAddQualDate-day'] + " " + data['assOnlyAddQualDate-month'] + " " + data['assOnlyAddQualDate-year']
+
+      res.redirect('/route')
+    })
+
 }
