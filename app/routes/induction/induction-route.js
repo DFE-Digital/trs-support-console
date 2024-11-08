399ab2f6409b3c00fb4aa0884a793c794f49d5ae
Tom <tommy_brown@me.com>
2024-11-08 17:23:46 +0000

    Removed req.body.newInductionStatus = null from induction-routes.js

--- app/routes/induction/induction-route.js
+++ app/routes/induction/induction-route.js
@@ -1,7 +1,7 @@
 module.exports = router => {
 
   router.post('/induction/status', (req, res) => {
-     req.body.newInductionStatus = null
+    //  req.body.newInductionStatus = null
     if (req.query.returnUrl) {
       res.redirect(req.query.returnUrl)
     } else {
