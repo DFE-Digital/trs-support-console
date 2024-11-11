c3247161beb2a4eba64e8342d05b8f0093a282aa
Tom <tommy_brown@me.com>
2024-11-11 16:04:37 +0000

    added induction/start-date and completion-date.html

--- app/routes/induction/induction-route.js
+++ app/routes/induction/induction-route.js
@@ -7,9 +7,36 @@ module.exports = router => {
     } else {
       if (req.body.newInductionStatus == "Exempt") {
         res.redirect('/induction/reason')
+      } else if (req.body.newInductionStatus == "Passed" || req.body.newInductionStatus == "Failed" || req.body.newInductionStatus == "Passed in Wales" || req.body.newInductionStatus == "Failed in Wales") {
+        res.redirect('/induction/start-date')
       } else {
         res.redirect('/induction/check')
       }
+    }      
+  })
+
+
+  router.post('/induction/start-date', (req, res) => {
+    if (req.query.returnUrl) {
+      res.redirect(req.query.returnUrl)
+    } else {
+      res.redirect('/induction/completion-date')  
+    }      
+  })
+
+  router.post('/induction/start-date', (req, res) => {
+    if (req.query.returnUrl) {
+      res.redirect(req.query.returnUrl)
+    } else {
+      res.redirect('/induction/completion-date')  
+    }      
+  })
+
+  router.post('/induction/completion-date', (req, res) => {
+    if (req.query.returnUrl) {
+      res.redirect(req.query.returnUrl)
+    } else {
+      res.redirect('/induction/check')  
     }      
   })
 
