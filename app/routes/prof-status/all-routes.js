e9a96375e3de45e17f5e94def56896f94c80fa14
Tom <tommy_brown@me.com>
2025-01-06 16:05:00 +0000

    Added app/views/route-complete/award-date.html         app/views/route-complete/induction-exemption.html

--- app/routes/prof-status/all-routes.js
+++ app/routes/prof-status/all-routes.js
@@ -1,19 +1,18 @@
-module.exports = router => {
+module.exports = (router) => {
+	router.post('/all-qts-routes', (req, res) => {
+			res.redirect('route-status')
+	})
 
-    router.post('/all-qts-routes', (req, res) => {
-          res.redirect('route-status')
-    })
+	router.post('/route-status', (req, res) => {
+			if (req.session.data.routeStatus === 'Awarded') {
+			res.redirect('/route-complete/end-date')
+			} else {
+			res.redirect('/route-add/start-date')
+			}
+	})
 
-    router.post('/route-status', (req, res) => {
-        if (req.session.data.routeStatus == 'Awarded') {
-            res.redirect('/route-complete/end-date')
-        } else {
-            res.redirect('/route-add/start-date')
-        }
-  })
-
-    router.get('/change-qts-route', (req, res) => {
-        req.session.data.chooseQtsRoute = {}
-        res.redirect('/all-qts-routes')
-    })
+	router.get('/change-qts-route', (req, res) => {
+			req.session.data.chooseQtsRoute = {}
+			res.redirect('/all-qts-routes')
+	})
 }
