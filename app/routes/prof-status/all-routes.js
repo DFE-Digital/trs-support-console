d27ce6e4d787c99e7044826312e6d8818b6d7aa5
Tom <tommy_brown@me.com>
2025-01-06 15:36:35 +0000

    Added views/route-add/ and views/route-complete/ DIRs + start and endDate templates

--- app/routes/prof-status/all-routes.js
+++ app/routes/prof-status/all-routes.js
@@ -1,10 +1,16 @@
 module.exports = router => {
 
     router.post('/all-qts-routes', (req, res) => {
-
           res.redirect('route-status')
-          
     })
+
+    router.post('/route-status', (req, res) => {
+        if (req.session.data.routeStatus == 'Awarded') {
+            res.redirect('/route-complete/end-date')
+        } else {
+            res.redirect('/route-add/start-date')
+        }
+  })
 
     router.get('/change-qts-route', (req, res) => {
         req.session.data.chooseQtsRoute = {}
