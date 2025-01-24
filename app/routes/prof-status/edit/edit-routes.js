1fd7da3e23d0109774fd83cc95c160bb191662d3
Tom <tommy_brown@me.com>
2025-01-24 15:01:54 +0000

    Moved change reason page before all datapoint pages in the edit journey. Age range and additional change reasons still not plugging in

--- app/routes/prof-status/edit/edit-routes.js
+++ app/routes/prof-status/edit/edit-routes.js
@@ -1,17 +1,100 @@
 module.exports = (router) => {
 
 	router.post('/route/edit/check', (req, res) => {
-		if (req.query.returnUrl) {
-			res.redirect(req.query.returnUrl)
-		} else {
-			res.redirect('/route/edit/change-reason')  
-		}      
+		res.redirect('/route/edit/change-reason')  
 	})
 
 	router.post('/route/edit/change-reason', (req, res) => {
+		if (req.query.returnUrl) {
 			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}  
 	})
 
+	router.post('/route/edit/all-qts-routes', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+		
+  })
+
+	router.post('/route/edit/route-status', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+		
+  })
+
+	router.post('/route/edit/start-date', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    	
+  })
+
+	router.post('/route/edit/end-date', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	router.post('/route/edit/training-provider', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	router.post('/route/edit/degree-type', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	router.post('/route/edit/country', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	router.post('/route/edit/end-date', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	router.post('/route/edit/key-stages', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	router.post('/route/edit/subjects', (req, res) => {
+		if (req.query.returnUrl) {
+			res.redirect(req.query.returnUrl)
+		} else {
+			res.redirect('/route/edit/check')  
+		}    
+  })
+
+	
 
 	
 	/////////////  FLASH //////////////
