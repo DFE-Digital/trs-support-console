6cd0b1600a3875f2f83b3b39ad1b76792e8868d6
Tom <tommy_brown@me.com>
2025-01-30 11:17:21 +0000

    added all delete code on delete-route.js

--- app/routes/prof-status/delete/delete-route.js
+++ app/routes/prof-status/delete/delete-route.js
@@ -4,15 +4,22 @@ module.exports = (router) => {
 		res.redirect('/route/delete/check')  
 	})
 
-	
-	
-
-	
-	/////////////  FLASH //////////////
-  router.post('/check-handler', (req, res) => {
-
-	req.flash('success', 'Route to professional status added')
-
-	res.redirect('/route')
+	router.post('/route/delete/check', (req, res) => {
+			delete req.session.data.activeRouteType
+			delete req.session.data.legacyRouteType
+      delete req.session.data.routeStatus
+			delete req.session.routeCompleteEndDate
+			delete req.session.routeAwardDate
+			delete req.session.data.hasRouteExemption
+			delete req.session.data.trainingProvider
+			delete req.session.data.degreeTypes
+			delete req.session.data.routeCountryOfTraining
+			delete req.session.data.ages
+			delete req.session.data.subjectChoices
+			// req.session.data.user = {}
+		req.flash('success', 'Route to professional status deleted')
+		res.redirect('/route')  
 	})
+
+
 }
