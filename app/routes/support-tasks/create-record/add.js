d0bbea865b8680c7b2740763c08250084c836a46
Tom <tommy_brown@me.com>
2025-02-10 16:02:04 +0000

    Added logis for duplicate record + added create-record/add/duplicates.html

--- app/routes/support-tasks/create-record/add.js
+++ app/routes/support-tasks/create-record/add.js
@@ -1,5 +1,16 @@
 module.exports = (router) => {
 
+  router.post('/create-record/add', (req, res) => {
+		res.redirect('/create-record/add/check')  
+	})
 
+  router.post('/create-record/add/check', (req, res) => {
+    let data = req.session.data
+    if (data.createRecordFullName === data.teachers.fullName || data.createRecordEmail === data.teachers.email || data.createRecordNationalInsuranceNumber === data.teachers.nationalInsuranceNumber) {
+      res.redirect('/create-record/add/duplicates')
+    }
+		res.redirect('/create-record/add/success')  
+	})
+  
 	
 }
