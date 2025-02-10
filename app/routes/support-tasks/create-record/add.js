37e19f6d279721495887680696567eee771df083
Tom <tommy_brown@me.com>
2025-02-10 16:20:29 +0000

    Changed to table formatting for duplicates on crete-record/add/duplicates.html

--- app/routes/support-tasks/create-record/add.js
+++ app/routes/support-tasks/create-record/add.js
@@ -6,7 +6,7 @@ module.exports = (router) => {
 
   router.post('/create-record/add/check', (req, res) => {
     let data = req.session.data
-    if (data.createRecordFullName === data.teachers.fullName || data.createRecordEmail === data.teachers.email || data.createRecordNationalInsuranceNumber === data.teachers.nationalInsuranceNumber) {
+    if (data.createRecordFullName === data.teachers[0].fullName || data.createRecordEmail === data.teachers[0].email || data.createRecordNationalInsuranceNumber === data.teachers[0].nationalInsuranceNumber) {
       res.redirect('/create-record/add/duplicates')
     }
 		res.redirect('/create-record/add/success')  
