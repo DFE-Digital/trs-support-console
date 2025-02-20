1658082a3a993b0ae3f85dfaf3368e946726f241
Tom <tommy_brown@me.com>
2025-02-20 17:27:58 +0000

    Added sass/components/_list.scss duplicates.json duplicates-card-layout-existing.njk duplicates-card-layout.njk And also created alternative card layour for multiple existing records in duplicates-cards.html

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,8 +1,10 @@
 const teachers = require('./teachers.json')
 const person = require('./person.json')
+const duplicates = require('./duplicates.json')
 
 module.exports = {
   // Insert values here
   teachers,
-  person
+  person,
+  duplicates
 }
