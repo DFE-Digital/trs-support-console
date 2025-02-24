eae51e3e2fb61cc160723f5865c0bb2c5aea1e47
Tom <tommy_brown@me.com>
2025-02-24 16:15:44 +0000

    Added table structure and for loop to show 'All tasks' in request-table.njk + changed primary and secondary nav to incluse 'All tasks

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,10 +1,12 @@
 const teachers = require('./teachers.json')
 const person = require('./person.json')
 const duplicates = require('./duplicates.json')
+const trnRequests = require('./trnRequests.json')
 
 module.exports = {
   // Insert values here
   teachers,
   person,
-  duplicates
+  duplicates,
+  trnRequests
 }
