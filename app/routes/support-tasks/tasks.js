39c9f03fa94fc41f4262800ac744390689c85922
Tom <tommy_brown@me.com>
2025-02-25 16:14:52 +0000

    Added from-trn-request/show + plugged in data for summary list

--- app/routes/support-tasks/tasks.js
+++ app/routes/support-tasks/tasks.js
@@ -1,8 +1,8 @@
 module.exports = (router) => {
 
-  router.get('/create-record/from-trn-request/tasks:taskId', (req, res) => {
+  router.get('/tasks/:taskId', (req, res) => {
 		let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
-    res.render('create-record/tasks/show', { task })
+    res.render('create-record/from-trn-request/show', { task })
 	})
 
 }
