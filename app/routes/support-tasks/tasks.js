943427cbb0f3d496eabdea71c57ea2e942b49b5e
Tom <tommy_brown@me.com>
2025-02-26 11:12:04 +0000

    Fixed route for passing TRN reqyuest to reson.html

--- app/routes/support-tasks/tasks.js
+++ app/routes/support-tasks/tasks.js
@@ -5,9 +5,10 @@ module.exports = (router) => {
     res.render('create-record/from-trn-request/show', { task })
 	})
 
-  router.post('/create-record/from-trn-request/check', (req, res) => {
-		let fullName = req.session.data.task.firstName + ' ' + req.session.data.task.lastName
-    res.render('create-record/from-trn-request/reason', { fullName })
+  router.get('/create-record/from-trn-request/reason/:taskId', (req, res) => {
+		let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
+    res.render('create-record/from-trn-request/reason', { task })
 	})
+
 
 }
