6df40da5633ceab6ba4ee30b3229cbb6ef830d1c
Tom <tommy_brown@me.com>
2025-02-28 17:37:35 +0000

    Filters code STILL to be fixed on add.js for filter panel

--- app/routes/support-tasks/create-record/from-trn-request/add.js
+++ app/routes/support-tasks/create-record/from-trn-request/add.js
@@ -1,6 +1,32 @@
 const _ = require('lodash')
 
 module.exports = (router) => {
+
+  /////////////  Filters   //////////
+  router.get('/support-tasks', (req, res) => {
+      
+    let tasks = req.session.data.tasks 
+
+    let selectedTaskFilters = _.get(req.session.data.filters, 'taskType')
+    
+    //User has selected a task type filter
+    if (_.get(selectedTaskFilters, 'length')) {
+
+      tasks = tasks.filter(task => {
+        let matchesTaskType = true
+
+        matchesTaskType = selectedTaskFilters.includes(task.taskType)
+        
+        return matchesTaskType
+      }) 
+    }
+
+    res.render('support-tasks/index', {
+      tasks
+    })
+  })
+
+
   ////////// get a single task //////////
   router.get('/support-tasks/:taskId', (req, res) => {
 		let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
@@ -49,34 +75,6 @@ module.exports = (router) => {
 
     // Redirect to the next page
     res.redirect('/create-record/from-trn-request')
-})   
-
-
-
-  
-  ///////////// Filters   //////////
-  router.get('/create-record/from-trn-request', (req, res) => {
-    
-    let tasks = req.session.data.tasks 
-
-    let selectedTaskFilters = _.get(req.session.data.filters, 'taskType')
-    //User has selected a task type filter
-    if (_.get(selectedTaskFilters, 'length')) {
-
-      tasks = tasks.filter(task => {
-        let matchesTaskType = true
-
-        matchesTaskType = selectedTaskFilters.includes(task.taskType)
-        
-
-        return matchesTaskType
-      }) 
-    }
-
-    res.render('create-record/from-trn-request/index',
-      {
-        tasks
-      })
-  })
+  })   
 
 }
