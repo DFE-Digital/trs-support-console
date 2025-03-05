eefcff4578cbf825f48956dc64ad5a7be278d33f
Tom <tommy_brown@me.com>
2025-03-05 12:10:14 +0000

    Post design crit changes made to create record from TRN request - No duplicates

--- app/routes/support-tasks/create-record/from-trn-request/non-matching/add.js
+++ app/routes/support-tasks/create-record/from-trn-request/non-matching/add.js
@@ -8,23 +8,27 @@ module.exports = (router) => {
     res.render('/support-tasks/show', { task })
 	})
 
-  //// This route is triggered from the button on 'show' page
-  router.get('/support-tasks/create-record/from-trn-request/no-match/:taskId/reason', (req, res) => {
-    let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
-    res.render('support-tasks/create-record/from-trn-request/no-match/reason', { task })
-  })
+    //// This route is triggered from the button on 'show' page
+    // router.get('/support-tasks/create-record/from-trn-request/no-match/:taskId/new', (req, res) => {
+    //   let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
+    //   res.render('support-tasks/create-record/from-trn-request/no-match/new', { task })
+    // }) 
 
-  
+  //// This route is triggered from the button on 'show' page
+  // router.get('/support-tasks/create-record/from-trn-request/no-match/:taskId/reason', (req, res) => {
+  //   let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
+  //   res.render('support-tasks/create-record/from-trn-request/no-match/reason', { task })
+  // })  
 
   //// THis route posts from the reason page
-  router.get('/support-tasks/create-record/from-trn-request/no-match/:taskId/check', (req, res) => {
-    let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
-    res.render('support-tasks/create-record/from-trn-request/no-match/check', {task})
-  })
+  // router.get('/support-tasks/create-record/from-trn-request/no-match/:taskId/check', (req, res) => {
+  //   let task = req.session.data.tasks.find(task => task.id === req.params.taskId)
+  //   res.render('support-tasks/create-record/from-trn-request/no-match/check', {task})
+  // })
 
 
   /////////////  FLASH //////////////
-  router.post('/support-tasks/create-record/from-trn-request/no-match/:taskId/new', (req, res) => {
+  router.get('/support-tasks/create-record/from-trn-request/no-match/:taskId/new', (req, res) => {
     
    
     
