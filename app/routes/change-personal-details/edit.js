6c56d6d4787a5276e898c065a9b88ee056f901ca
Tom <tommy_brown@me.com>
2025-03-21 17:10:36 +0000

    Details and check fixed. Stil to fix redirect to index - personal details

--- app/routes/change-personal-details/edit.js
+++ app/routes/change-personal-details/edit.js
@@ -5,31 +5,46 @@ module.exports = (router) => {
     // Merge form data with existing person data
     const updatedData = {
       ...personData[0],
-      firstName: req.body.personalDetailsFirstName || personData[0].firstName,
-      middleName: req.body.personalDetailsMiddleName || personData[0].middleName,
-      lastName: req.body.personalDetailsLastName || personData[0].lastName,
-      dateOfBirthDay: req.body.personalDetailsDateOfBirth?.day || personData[0].dateOfBirthDay,
-      dateOfBirthMonth: req.body.personalDetailsDateOfBirth?.month || personData[0].dateOfBirthMonth,
-      dateOfBirthYear: req.body.personalDetailsDateOfBirth?.year || personData[0].dateOfBirthYear,
-      email: req.body.personalDetailsEmail || personData[0].email,
-      mobile: req.body.personalDetailsPhoneNumber || personData[0].mobile,
-      nationalInsurance: req.body.personalDetailsNationalInsuranceNumber || personData[0].nationalInsurance,
-      gender: req.body.personalDetailsSex || personData[0].gender,
-      createRecordReason: req.body.createRecordReason || null,
-      evidenceFile: req.body.evidenceFile || null,
+      firstName: req.body.personalDetailsFirstName,
+      middleName: req.body.personalDetailsMiddleName,
+      lastName: req.body.personalDetailsLastName,
+      dateOfBirthDay: req.body.personalDetailsDateOfBirth?.day,
+      dateOfBirthMonth: req.body.personalDetailsDateOfBirth?.month,
+      dateOfBirthYear: req.body.personalDetailsDateOfBirth?.year,
+      email: req.body.personalDetailsEmail,
+      mobile: req.body.personalDetailsPhoneNumber,
+      nationalInsurance: req.body.personalDetailsNationalInsuranceNumber,
+      gender: req.body.personalDetailsSex,
+      createRecordReason: req.body.createRecordReason,
+      evidenceFile: req.body.evidenceFile,
     };
 
     req.session.updatedPersonData = updatedData; // Store updated data in session
     res.redirect('/personal-details/check');
-  });
-
-  router.get('/personal-details/check', (req, res) => {
-    const updatedData = req.session.updatedPersonData || personData[0]
-    res.render('personal-details/index', { updatedData })
+		console.log(updatedData)
   })
 
-  router.get('/personal-details/check', (req, res) => {
-    const data = req.session.updatedPersonData || personData[0]
-    res.render('personal-details/check', { data })
+	
+  router.post('/personal-details/check', (req, res) => {
+		// Merge form data with existing person data
+    const updatedData = {
+      ...personData[0],
+      firstName: req.body.personalDetailsFirstName,
+      middleName: req.body.personalDetailsMiddleName,
+      lastName: req.body.personalDetailsLastName,
+      dateOfBirthDay: req.body.personalDetailsDateOfBirth?.day,
+      dateOfBirthMonth: req.body.personalDetailsDateOfBirth?.month,
+      dateOfBirthYear: req.body.personalDetailsDateOfBirth?.year,
+      email: req.body.personalDetailsEmail,
+      mobile: req.body.personalDetailsPhoneNumber,
+      nationalInsurance: req.body.personalDetailsNationalInsuranceNumber,
+      gender: req.body.personalDetailsSex,
+      createRecordReason: req.body.createRecordReason,
+      evidenceFile: req.body.evidenceFile,
+    }
+		req.session.updatedPersonData = updatedData; // Store updated data in session
+    res.redirect('/personal-details/index')
   })
+
+  
 }
